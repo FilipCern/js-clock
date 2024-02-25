38067515a993ed14c454622370b01c5e964930a6
FilipCern <filip.cernohorsky48@gmail.com>
2024-02-25 17:52:24 +0100

    adding clock image

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hoursHand = document.querySelector('.hour-hand');
+const secondsHand = document.querySelector('.seconds-hand');
+const minsHand = document.querySelector('.minutes-hand');
+const hoursHand = document.querySelector('.hours-hand');
 
 function setDate () {
     const now = new Date();
     
     const seconds = now.getSeconds();
     const secondsDegrees= ((seconds/60) * 360) + 90;
-    secondHand.style.transform= `rotate(${secondsDegrees}deg)`;
+    secondsHand.style.transform= `rotate(${secondsDegrees}deg)`;
     console.log(seconds);
 
     const minutes = now.getMinutes();
